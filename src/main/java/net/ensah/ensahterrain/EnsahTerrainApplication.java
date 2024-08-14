53aabb830cc79a2d907454623c44f1e71719545f
abdelilah Saouiri <abdosaouiri757@gmail.com>
2024-08-14 22:22:15 +0100

    first commit

---
+++ src/main/java/net/ensah/ensahterrain/EnsahTerrainApplication.java
@@ -0,0 +1,13 @@
+package net.ensah.ensahterrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsahTerrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnsahTerrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ensah/ensahterrain/EnsahTerrainApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ensah.ensahterrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsahTerrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
